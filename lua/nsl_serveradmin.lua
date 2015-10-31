583e349fc700f213f38279a63c0f54468eb38059
Matt <mkreider@gmail.com>
2015-10-31 04:19:56 -0400

    hooks for vanilla server admin commands

---
+++ lua/nsl_serveradmin.lua
@@ -0,0 +1,85 @@
+// Natural Selection League Plugin
+// Source located at - https://github.com/xToken/NSL
+// lua\nsl_serveradmin.lua
+// - Dragon
+
+//NSL Server Admin Hooks
+
+local function GetUpValue(origfunc, name)
+
+	local index = 1
+	local foundValue = nil
+	while true do
+	
+		local n, v = debug.getupvalue(origfunc, index)
+		if not n then
+			break
+		end
+		
+		-- Find the highest index matching the name.
+		if n == name then
+			foundValue = v
+		end
+		
+		index = index + 1
+		
+	end
+	
+	return foundValue
+	
+end
+
+function ReplaceLocals(originalFunction, replacedLocals)
+
+    local numReplaced = 0
+    for name, value in pairs(replacedLocals) do
+    
+        local index = 1
+        local foundIndex = nil
+        while true do
+        
+            local n, v = debug.getupvalue(originalFunction, index)
+            if not n then
+                break
+            end
+            
+            -- Find the highest index matching the name.
+            if n == name then
+                foundIndex = index
+            end
+            
+            index = index + 1
+            
+        end
+        
+        if foundIndex then
+        
+            debug.setupvalue(originalFunction, foundIndex, value)
+            numReplaced = numReplaced + 1
+            
+        end
+        
+    end
+    
+    return numReplaced
+    
+end
+
+local GetClientCanRunCommand = GetUpValue(CreateServerAdminCommand, "GetClientCanRunCommand")
+
+local function NSLGetClientCanRunCommand(client, commandName, printWarning)
+
+		if not client then return end
+		local NS2ID = client:GetUserId()
+		local canRun = false
+		if GetIsNSLRef(NS2ID) then
+			canRun = GetCanRunCommandviaNSL(NS2ID, commandName)
+		end
+        if not canRun then
+			return GetClientCanRunCommand(client, commandName, printWarning)
+		end
+        return canRun
+        
+    end
+
+ReplaceLocals(CreateServerAdminCommand, { GetClientCanRunCommand = NSLGetClientCanRunCommand })
