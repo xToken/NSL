d710fe485c8c65058e392cf480f7b9f8e4b59479
Matt <mkreider@gmail.com>
2016-02-08 12:43:17 -0500

    updated for new config loader

--- lua/nsl_consistencyenforcer_server.lua
+++ lua/nsl_consistencyenforcer_server.lua
@@ -4,56 +4,59 @@
 // - Dragon
 
 //Hmm lets see what kind of craziness THIS can cause...
-
 local ConsistencyApplied = false
-local consistencyLocalFile = "configs/nsl_leagueconfig.json"
+local consistencyBackupFile = "configs/nsl_consistencybackupconfig.json"
 
-local function ApplyNSLConsistencyConfig()
+local function ApplyNSLConsistencyConfig(configLoaded)
 
-	local consistencyConfig = GetNSLConfigValue("ConsistencyConfig")
+	if configLoaded == "all" or configLoaded == "consistency" then
 	
-	if not consistencyConfig then
-		local file = io.open(consistencyLocalFile, "r")
-		if file then
-			consistencyConfig = json.decode(file:read("*all"))
-			file:close()
+		local consistencyConfig = GetNSLConfigValue("ConsistencyConfig")
+		if not consistencyConfig then
+			//This *should* be impossible.
+			local file = io.open(consistencyBackupFile, "r")
+			if file then
+				consistencyConfig = json.decode(file:read("*all"))
+				file:close()
+			end
+			Shared.Message("NSL - Using backup ConsistencyConfig.")
 		end
-	end
-		
-	if consistencyConfig and not ConsistencyApplied then
+			
+		if consistencyConfig and not ConsistencyApplied then
 
-		local startTime = Shared.GetSystemTime()
-		//First, remove ANY established config
-		Server.RemoveFileHashes("*.*")
-		
-		if type(consistencyConfig.check) == "table" then
-			local check = consistencyConfig.check
-			for c = 1, #check do
-				local numHashed = Server.AddFileHashes(check[c])
-				Shared.Message("Hashed " .. numHashed .. " " .. check[c] .. " files for consistency")
+			local startTime = Shared.GetSystemTime()
+			//First, remove ANY established config
+			Server.RemoveFileHashes("*.*")
+			
+			if type(consistencyConfig.check) == "table" then
+				local check = consistencyConfig.check
+				for c = 1, #check do
+					local numHashed = Server.AddFileHashes(check[c])
+					Shared.Message("Hashed " .. numHashed .. " " .. check[c] .. " files for consistency")
+				end
 			end
-		end
 
-		if type(consistencyConfig.ignore) == "table" then
-			local ignore = consistencyConfig.ignore
-			for c = 1, #ignore do
-				local numHashed = Server.RemoveFileHashes(ignore[c])
-				Shared.Message("Skipped " .. numHashed .. " " .. ignore[c] .. " files for consistency")
+			if type(consistencyConfig.ignore) == "table" then
+				local ignore = consistencyConfig.ignore
+				for c = 1, #ignore do
+					local numHashed = Server.RemoveFileHashes(ignore[c])
+					Shared.Message("Skipped " .. numHashed .. " " .. ignore[c] .. " files for consistency")
+				end
 			end
-		end
-		
-		if type(consistencyConfig.restrict) == "table" then
-			local check = consistencyConfig.restrict
-			for c = 1, #check do
-				local numHashed = Server.AddRestrictedFileHashes(check[c])
-				Shared.Message("Hashed " .. numHashed .. " " .. check[c] .. " files for consistency")
+			
+			if type(consistencyConfig.restrict) == "table" then
+				local check = consistencyConfig.restrict
+				for c = 1, #check do
+					local numHashed = Server.AddRestrictedFileHashes(check[c])
+					Shared.Message("Hashed " .. numHashed .. " " .. check[c] .. " files for consistency")
+				end
 			end
+			
+			local endTime = Shared.GetSystemTime()
+			Print("NSL - Enhanced Consistency checking took " .. ToString(endTime - startTime) .. " seconds.")
+			ConsistencyApplied = true
+			
 		end
-		
-		local endTime = Shared.GetSystemTime()
-		Print("NSL Enhanced Consistency checking took " .. ToString(endTime - startTime) .. " seconds.")
-		ConsistencyApplied = true
-		
 	end
 	
 end
