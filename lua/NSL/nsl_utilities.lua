932b16eea78a0798a9fd237bc5b7dd3ada11ad03
Dragon <mkreider@gmail.com>
2019-06-23 21:52:23 -0400

    Part of vanilla, add for compat with older builds if needed

--- lua/NSL/nsl_utilities.lua
+++ lua/NSL/nsl_utilities.lua
@@ -38,3 +38,41 @@ function Class_ReplaceMethod(className, methodName, method)
 	return original
 
 end
+
+-- This doesnt exist in older builds
+if not debug.getupvaluex then
+	local old = debug.getupvalue
+    local function getupvalue(f, up, recursive)
+        if type(up) ~= "string" then
+            return old(f, up)
+        end
+
+        if recursive == nil then
+            recursive = true
+        end
+
+        local funcs   = {}
+        local i, n, v = 0
+        repeat
+            i = i + 1
+            n, v = old(f, i)
+            if recursive and type(v) == "function" then
+                table.insert(funcs, v)
+            end
+        until
+            n == nil or n == up
+
+        -- Do a recursive search
+        if n == nil then
+            for _, subf in ipairs(funcs) do
+                v, f, i = getupvalue(subf, up)
+                if f ~= nil then
+                    return v, f, i
+                end
+            end
+        elseif n == up then
+            return v, f, i
+        end
+    end
+    debug.getupvaluex = getupvalue
+end
