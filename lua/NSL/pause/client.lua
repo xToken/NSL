b57491f359710974b90aca7c5935b0c216196f27
Dragon <mkreider@gmail.com>
2020-06-12 22:27:29 -0400

    Fix for scoreboard changes in 332

--- lua/NSL/pause/client.lua
+++ lua/NSL/pause/client.lua
@@ -105,7 +105,6 @@ function AddGUIScriptToPausedUpdates(GUIClassName)
     table.insert(kPausedUpdateScripts, GUIClassName)
 end
 
-local playerData
 local originalGUIManagerUpdate
 originalGUIManagerUpdate = Class_ReplaceMethod("GUIManager", "Update", 
 	function(self, deltaTime)
@@ -130,12 +129,6 @@ originalGUIManagerUpdate = Class_ReplaceMethod("GUIManager", "Update",
 			end
 			scoreboardUpdate = math.max(0, scoreboardUpdate - deltaTime)
 			if scoreboardUpdate == 0 then
-				if not playerData then
-					playerData = debug.getupvaluex(Scoreboard_ReloadPlayerData, "playerData")
-				end
-				for id, data in pairs(playerData) do
-					data.LastUpdateTime = data.LastUpdateTime - kPausedScoreboardUpdateRate - deltaTime
-				end
 				Scoreboard_ReloadPlayerData()
 				scoreboardUpdate = kPausedScoreboardUpdateRate
 			end
